b76dd3a4d227bba5fba44d10c1ea72787197b25d
Wasim Adil <waseemadilcode@gmail.com>
2025-03-08 21:40:03 +0500

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,58 +2,19 @@ const epxree = require('express');
 const app = epxree();
 
 
-app.use("/user", (req, res) =>{
-    res.send("Hahahahahhahah")
-})
+app.get("/user/:userId/:name/:password", (req,res) =>{
+    console.log(req.params);
 
-app.get('/' ,(req, res) => {
-    res.send('Hello from the server!')
-})
-
-// app.use("/" , (req ,res) => {
-//     res.send("Hello from main route ")
-// })
-
-app.use("/hello" , (req, res) => {
-    
-    res.send('Hello hello Hello')
-})
-
-
-app.get("/hello/2" , (req, res) => {
-    
-    res.send('Hello hello test')
-})
-
-
-app.get("/user" , (req,res) =>{
     res.send({
-        firstName : "Wasim",
-        lastName : "Adil"
-
+        firstname: "Wasim",
+        lastname: "Adil"
     })
+
 })
-
-
-app.post("/user" , (req,res) => {
-    
-    console.log("Data are saved in database");
-    res.send("Data are saved in database")
-})
-
-
-app.delete("/user", (req,res) =>{
-    res.send("Delete Successfully ... ")
-})
-
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
 
-
-// app.use("/user", (req, res) =>{
-//     res.send("Hahahahahhahah")
-// })
 
 
