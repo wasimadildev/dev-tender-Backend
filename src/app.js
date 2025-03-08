6b81a3dc651c1b67f6905ae728a253de5936006d
Wasim Adil <waseemadilcode@gmail.com>
2025-03-08 23:08:56 +0500

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,19 +2,23 @@ const epxree = require('express');
 const app = epxree();
 
 
-app.get("/user/:userId/:name/:password", (req,res) =>{
-    console.log(req.params);
 
-    res.send({
-        firstname: "Wasim",
-        lastname: "Adil"
-    })
+app.use("/user", [(req, res , next) =>{
 
-})
+ console.log("Handling the route user");
+
+//  res.send("Responsed from user")
+ next();
+}, (req,res) =>{
+    console.log("Handling the route user 2");
+    res.send("Responsed from user 2")
+}])
+
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
+
 
 
 
